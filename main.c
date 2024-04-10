96126af768fba2b5e94331edde9eef6af136aaad
Brayden Edwards <edwabray@oregonstate.edu>
2024-04-10 14:12:30 -0700

    Initial functionality for reading the sample file

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+    const int CHAR_SIZE = 1000;
+    char fileContent[CHAR_SIZE];
+    FILE *fileHandler = fopen(argv[1], "r");
+
+    if (fileHandler == NULL) {
+        perror("Error loading file");
+    }
+    else {
+        if (fgets(fileContent, CHAR_SIZE, fileHandler) != NULL) {
+            puts(fileContent);
+        }
+        fclose(fileHandler);
+    }
+
+
+
     printf("%d", argc);
     printf("%s", argv[0]);
     printf("\n");
-    printf("%s", argv[1]);
+    printf("%p", &fileContent);
     printf("\n");
     printf("Hello, World!\n");
     return 0;
