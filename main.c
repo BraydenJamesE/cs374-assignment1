6a7a9e195821256d437e47c84e22eeb88532f636
Brayden Edwards <edwabray@oregonstate.edu>
2024-04-16 20:16:23 -0700

    Properly storing values in the struct and linked list created

--- main.c
+++ main.c
@@ -6,19 +6,23 @@
 #define DELIMITER ","
 
 const int NUMBER_OF_MOVIES = 50;
-const int CHAR_SIZE = 1000;
-const int LANGUAGE_MAX_LENGTH = 20;
+const int CHAR_SIZE = 1001;
+const int LANGUAGE_MAX_LENGTH = 21;
 const int MAX_NUM_OF_LANGUAGES = 5;
+int actualNumberOfMovies = 0;
 
 struct MovieData {
     char *title;
     int year;
     char languages[MAX_NUM_OF_LANGUAGES][LANGUAGE_MAX_LENGTH];
     float rating;
+    struct MovieData *leftNeighbor;
+    struct MovieData *rightNeighbor;
 };
 
 int main(int argc, char **argv) {
-    struct MovieData *movies = malloc(NUMBER_OF_MOVIES * sizeof(struct MovieData));
+    struct MovieData *head = malloc(sizeof(struct MovieData));
+    //struct MovieData *movies = malloc(NUMBER_OF_MOVIES * sizeof(struct MovieData));
     char fileContent[CHAR_SIZE];
     FILE *fileHandler = fopen(argv[1], "r");
 
@@ -28,8 +32,18 @@ int main(int argc, char **argv) {
     else {
         bool headerRead = false;
         int index = 0;
+        struct MovieData *currentMovie = malloc(sizeof(struct MovieData));
         while (true) {
             if (fgets(fileContent, CHAR_SIZE, fileHandler) != NULL) {
+                if (index == 0) {
+                    currentMovie = head;
+                } else {
+                    struct MovieData *newMovie = malloc(sizeof(struct MovieData));
+                    currentMovie->rightNeighbor = newMovie;
+                    newMovie->leftNeighbor = currentMovie;
+                    currentMovie = newMovie;
+                }
+
                 if (headerRead == false) { // skipping the header of the file
                     headerRead = true;
                     continue;
@@ -40,43 +54,63 @@ int main(int argc, char **argv) {
                     size_t titleLength = strlen(token);
                     char *title = (char *)malloc((titleLength + 1) * sizeof(char));
                     strcpy(title, token);
-                    movies[index].title = title;
+                    currentMovie->title = title;
                 }
 
                 token = strtok(NULL, DELIMITER); // reading the year
                 if(token != NULL){
-                    movies[index].year = atoi(token);
-                    printf("%d->", movies[index].year);
+                    currentMovie->year = atoi(token);
                 }
 
                 token = strtok(NULL, DELIMITER); // reading the languages
-                if(token != NULL)
-                    printf("%s    ", token);
+                if(token != NULL) {
+                    int languageCharIndex = 0;
+                    int numberOfLanguagesIndex = 0;
+                    int startIndex = 1;
+                    int endIndex = 0;
+                    bool breakLoop = false;
+                    while (true) {
+                        if (token[languageCharIndex] == ';' || token[languageCharIndex] == ']') {
+                            if (token[languageCharIndex] == ']')
+                                breakLoop = true;
+                            int substringLength = endIndex - startIndex;
+                            char *substringStart = token + startIndex;
+                            char substring[substringLength + 1];
+                            strncpy(substring, substringStart, substringLength);
+                            substring[substringLength] = '\0';
+                            strcpy(currentMovie->languages[numberOfLanguagesIndex], substring);
+                            numberOfLanguagesIndex++;
+                            startIndex = endIndex + 1;
+                        }
+                        endIndex++;
+                        languageCharIndex++;
+                        if (breakLoop)
+                            break;
+                    }
+                }
 
                 token = strtok(NULL, DELIMITER); // reading the rating
-                if(token != NULL)
-                    printf("%s    \n", token);
+                if(token != NULL) {
+                    float rating = atof(token);
+                    currentMovie->rating = rating;
+                }
                 index++;
             } else {
+                actualNumberOfMovies = index;
+                head->leftNeighbor = currentMovie;
+                currentMovie->rightNeighbor = head;
                 break;
             }
         }
         fclose(fileHandler);
-
     }
+    struct MovieData *printMovie = head->leftNeighbor;
+    for(int i = 0; i < actualNumberOfMovies; i++) {
+        printf("%d: %s \n", i + 1, printMovie->title);
+        printMovie = printMovie->leftNeighbor;
+    }
+//    printf("%s -> %d -> %s -> %.1f", head->leftNeighbor->title, head->year, head->languages[1], head->rating);
+//    printf("\n Actual Number of Movies: %d", actualNumberOfMovies);
 
-
-
-    printf("%d", argc);
-    printf("%s", argv[0]);
-    printf("\n");
-    printf("%p", fileContent);
-    printf("\n");
-    printf("Hello, World!\n");
-
-
-    // free memory
-    //free(movies);
-    //free(movies[0].title);
     return 0;
 }
